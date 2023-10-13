ae490eb47d3fef13e204877a3ae9c118826c49ac
Sam Mitchell <samuel.i.mitchell@gmail.com>
2023-10-12 22:14:30 -0400

    done for now.  Seems to work properly git status '
    l;'asdfj;klasdfjkl;
    k

--- rpg.py
+++ rpg.py
@@ -7,22 +7,50 @@ class Character:
 
     # alive - to check if warrior is alive.  an instance method
     def alive(self):
-        return self.health >= 0
+        return self.health > 0
+    
+     # greet - an instance method
+    def greet(self, other):
+        print()
+        print(f"You are a mighty {self.name}.\nYou wield the Sword of Destiny and you are here only to win.")
+        print(f"You have {self.health} health and {self.power} power.")
+        print(f"The {other.name} has {other.health} health and {other.power} power.")
+
+    def print_status(self, other):
+        if other.alive():
+            print()
+            print (f"{self.name} health: {self.health}")
+        if self.alive():
+            print()
+            print (f"{other.name} health: {other.health}")
+        else:
+            print()
+            print(f"The {self.name} has died.  The world is now a much darker place.")
+            print()
 
     # attack - an instance method
     def attack(self, other):
         other.health -= self.power
-        print(f"{self.name} do %d damage to the {other.name}." % other.power)
-        if goblin.health <= 0:
-            print(f"The {other.name} is dead.")
+
+        if self.alive():
+            print()
+            print(f"{self.name} does %d damage to the {other.name}." % self.power)
+    
+        else:
+            print()
+            print(f"The {self.name} has died.  Peasants and minstrels will sing songs of your victory here for all time.  Long live the {other.name}!")  
+            print() 
+        
 
     # do nothing - an instance method
     def nothing(self):
         pass
 
     # flee - an instance method
-        def flee(self):
-            print(f"You have chosen to {flee}.  Goodbye, coward.  You're a crappy hero.")
+    def flee(self):
+        print()
+        print(f"{self.name} has chosen to flee.  Goodbye, coward.  You're a crappy hero.")
+        print()
 
     
 # child class constructor
@@ -30,16 +58,10 @@ class Hero(Character):
     def __init__(self, name, health, power):
         super().__init__(name, health, power)   
 
-    # greet - an instance method
-    def greet(self, other):
-        print(f"You are a mighty {self.name}.\nYou wield the Sword of Destiny and you are here only to win.")
-        print(f"You have {self.health} health and {self.power} power.")
-        print(f"The {other.name} has {other.health} health and {other.power} power.")
-
-
+   
 # child class constructor
 class Goblin(Character):
-    def __init__(self, name, health,power):
+    def __init__(self, name, health, power):
         super().__init__(name, health, power)
 
 
@@ -50,7 +72,7 @@ hero = Hero("Hero", 10, 5)
 goblin = Goblin("Goblin", 6, 2)
 
 # call the method greet
-hero.greet()
+hero.greet(goblin)
 
 while hero.alive() and goblin.alive():
         print()
@@ -64,29 +86,31 @@ while hero.alive() and goblin.alive():
             hero.attack(goblin)
             goblin.attack(hero)
             if goblin.alive():
-                print (f"Goblin health: {goblin.health}")
-            if hero.alive():
-                print (f"Hero health: {hero.health}")
+                hero.print_status(goblin)
+            # if hero.alive():
+            #     print()
+            #     print (f"Hero health: {hero.health}")
+            # if goblin.alive():
+            #     print()
+            #     print (f"Goblin health: {goblin.health}")
+            
 
         elif user_input == "2":
             hero.nothing()
             goblin.attack(hero)
+            # print()
             if goblin.alive():
-                print (f"Goblin health: {goblin.health}")
-            if hero.alive():
-                print (f"Hero health: {hero.health}")
+                hero.print_status(goblin)
+            # if hero.alive():
+            #     hero.print_status(goblin)
+            # else:
+            #     print()
+            #     print (f"The {hero.name} is dead.  The world is a darker place now.")
 
         elif user_input == "3":
             hero.flee()
             break
 
         else:
+            print()
             print("Invalid input %r" % user_input)
-
-        # if goblin.alive():
-            # Goblin attacks hero
-        
-        # hero.health -= goblin.power
-        # print("The goblin does %d damage to you." % goblin.power)
-        # if hero.health <= 0:
-        #     print("You are dead.  The world is a darker place now.")
